eb1c1e59437969da52e3a697141e4b957dd4bc24
honey-2020 <57524774+honey-2020@users.noreply.github.com>
2020-12-03 18:24:45 +0100

    struttura di the project

--- aemilia-theme/page-project.php
+++ aemilia-theme/page-project.php
@@ -6,7 +6,16 @@
 ;?>
 
 <?php get_header();?>
-<h1>Project</h1>
+
+<div class="page_project_container">
+    <h1 class="project_title"><?php the_title(); ?></h1>
+    <img  class="img" src=<?php echo wp_get_attachment_url(31);?> alt="">
+    
+    <p class="project_content"><?php the_content(); ?></p>
+
+    <img  class="img rotate" src=<?php echo wp_get_attachment_url(31);?> alt="">
+
+</div>
+
 </body>
 </html>
-
