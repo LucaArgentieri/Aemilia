8991ea55e13f01518d65c02d212eeb3ecf36403d
LucaArgentieri <Lucaargentieri-98@hotmail.it>
2021-01-07 14:44:02 +0100

    Update map mobile

--- aemilia-theme/inc/script/roadmap.js
+++ aemilia-theme/inc/script/roadmap.js
@@ -1,9 +1,18 @@
 let line = document.querySelector('.map');
+let lineContainer = document.querySelectorAll('.line-container')
+
+window.addEventListener('resize', e => {
+    e.preventDefault()
+    location.reload()
+})
+
 
 function set_line() {
     let angle_deg;
     let wind_width = window.innerWidth;
     let wind_height = window.innerHeight;
+
+
     if (wind_width >= wind_height) {
         line.style.minWidth = `95vw`;
     } else if (wind_width < wind_height) {
@@ -12,6 +21,15 @@ function set_line() {
     angle_deg = Math.atan(wind_height / wind_width) * (180 / Math.PI);
     line.style.transform = `rotate(${angle_deg - 5}deg)`;
 
+
+    if (wind_width <= 900) {
+        lineContainer.forEach(e => {
+            e.classList.add('line-container-grey')
+        })
+        angle_deg = 90
+        line.style.minWidth = `70vh`;
+        line.style.transform = `rotate(${angle_deg}deg)`;
+    }
 }
 
 
